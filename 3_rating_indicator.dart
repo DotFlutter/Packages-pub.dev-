f5c063d67995c61290150f3aae8ae8e8f2cfd603
Maria Arshad <mobile.app.developer963@gmail.com>
2025-10-29 11:26:52 +0500

    Create 3_rating_indicator.dart

---
+++ 3_rating_indicator.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:m_store/utils/constants/colors.dart';
+
+class MRatingBarIndicator extends StatelessWidget {
+  const MRatingBarIndicator({
+    super.key,
+    required this.rating,
+  });
+  final double rating;
+  @override
+  Widget build(BuildContext context) {
+    return RatingBarIndicator(
+      rating: rating,
+      itemSize: 20,
+      unratedColor: MColors.grey,
+      itemBuilder: (context, index) => const Icon(Iconsax.star1, color: MColors.primary),
+    );
+  }
+}
